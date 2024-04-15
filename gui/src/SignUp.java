79dbdfb234ad4d09c9f909e9a1ba4f1de31c9817
Ameesh Sethi <ameesh.sethi@gmail.com>
2024-04-15 06:35:22 +0530

    added signup page

---
+++ gui/src/SignUp.java
@@ -0,0 +1,64 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class SignUp extends JDialog {
+    private JTextField tfUsername;
+    private JPasswordField pfPassword;
+    private JPasswordField pfConfirmPassword;
+    private JButton btnRegister;
+    private JButton btnCancel;
+    private JPanel SignUpPanel;
+    private JTextField tfUserName;
+
+    public SignUp(JFrame parent) {
+        super(parent);
+        setTitle("Sign Up");
+        setContentPane(SignUpPanel);
+        pack();
+        setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+        setMinimumSize(SignUpPanel.getMinimumSize());
+        setModal(true);
+        setLocationRelativeTo(parent);
+
+        btnRegister.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                registerUser();
+            }
+        });
+
+        btnCancel.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                dispose();
+            }
+        });
+
+        setVisible(true);
+    }
+
+    private void registerUser() {
+        String username = tfUserName.getText();
+        String password = new String(pfPassword.getPassword());
+        String confirmPassword = new String(pfConfirmPassword.getPassword());
+
+        if (username.isEmpty() || password.isEmpty() || confirmPassword.isEmpty()) {
+            JOptionPane.showMessageDialog(null, "Please fill in all fields!");
+        } else if (!password.equals(confirmPassword)) {
+            JOptionPane.showMessageDialog(null, "Passwords do not match!");
+        } else {
+            JOptionPane.showMessageDialog(null, "Registration successful!");
+        }
+    }
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+
+    }
+
+    public static void main(String[] args) {
+        new SignUp(null);
+    }
+}
