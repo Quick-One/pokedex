002e0cd1a9af78fd628e36be273c3d0b2ecbdc9d
Ameesh Sethi <ameesh.sethi@gmail.com>
2024-04-15 19:28:28 +0530

    added MainMenu gui

---
+++ gui/src/MainMenu.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+public class MainMenu {
+    private JButton searchPokemonButton;
+    private JButton addButton;
+    private JButton button3;
+    private JButton button4;
+    private JLabel headerText;
+    private JPanel MainMenuPanel;
+    private JLabel salutationText;
+    private JButton updateButton;
+    private JButton deleteButton;
+}
